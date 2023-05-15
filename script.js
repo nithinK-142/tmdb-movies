a4d0f844a63e984fc8f7b2ae0431ef770d574d2f
nithin <nithin.sagar.359@gmail.com>
2023-05-15 19:17:59 +0530

    initial commit.

---
+++ script.js
@@ -0,0 +1,80 @@
+const api_key = "YOUR_TMBD_API";
+
+const APIURL_POPULAR = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + api_key;
+const APIURL_TOP_RATED = "https://api.themoviedb.org/3/movie/top_rated?&api_key=" + api_key + "&page=1";
+
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+
+$(document).ready(function() {
+    const label = $("#label");
+    const main = $("#main");
+    let originalLabelText = label.text();
+
+    label.click(function() {
+        toggleLabelText();
+        toggleMainContent();
+    });
+
+    function toggleLabelText() {
+        if (label.text() === originalLabelText) {
+            label.text("top rated movies");
+        } else {
+            label.text(originalLabelText);
+        }
+    }
+
+    function toggleMainContent() {
+        main.empty();
+
+        if (label.text() === "top rated movies") {
+            getMovies(APIURL_TOP_RATED);
+        } else {
+            getMovies(APIURL_POPULAR);
+        }
+    }
+
+    async function getMovies(url) {
+        const resp = await fetch(url);
+        const respData = await resp.json();
+        showMovies(respData.results);
+    }
+
+    function showMovies(movies) {
+        movies.forEach((movie) => {
+            const { poster_path, title, vote_average, overview } = movie;
+
+            const movieEl = $("<div>").addClass("movie");
+
+            movieEl.html(`
+                <img
+                    src="${IMGPATH + poster_path}"
+                    alt="${title}"
+                />
+                <div class="movie-info">
+                    <h3>${title}</h3>
+                    <span class="${getClassByRate(
+                        vote_average
+                    )}">${vote_average}</span>
+                </div>
+                <div class="overview">
+                    <h3>Overview:</h3>
+                    ${overview}
+                </div>
+            `);
+
+            main.append(movieEl);
+        });
+    }
+
+    function getClassByRate(vote) {
+        if (vote >= 8) {
+            return "green";
+        } else if (vote >= 5) {
+            return "orange";
+        } else {
+            return "red";
+        }
+    }
+
+    getMovies(APIURL_POPULAR);
+});
